b4dbaebcaf9fd997225b676e697ba03f090e12a8
Emu <emulatoriu@gmail.com>
2025-01-27 08:53:59 +0100

    add example for text blocks

---
+++ AsciiPrinter/src/main/java/io/AsciiPrinter.java
@@ -0,0 +1,12 @@
+package io;
+
+public class AsciiPrinter {
+	public void printAsciiText() {		
+		String asciiText = """
+				(\\,-------------------------'()'--o
+				(_    _Hard work pays off_    /~" 
+				(_)_)                   (_)_)
+				""";
+		System.out.println(asciiText);
+	}
+}
---
+++ AsciiPrinter/src/main/java/logic/Application.java
@@ -0,0 +1,12 @@
+package logic;
+
+import io.AsciiPrinter;
+
+public class Application {
+
+	public static void main(String[] args) {
+		AsciiPrinter asciiPrinter = new AsciiPrinter();
+		asciiPrinter.printAsciiText();
+	}
+
+}
