d6dffd87bec570807ca41be2d13404a4c7f18fe2
Emu <emulatoriu@gmail.com>
2025-01-27 09:00:31 +0100

    add example for string formatting

--- AsciiPrinter/src/main/java/io/AsciiPrinter.java
+++ AsciiPrinter/src/main/java/io/AsciiPrinter.java
@@ -4,9 +4,9 @@ public class AsciiPrinter {
 	public void printAsciiText() {		
 		String asciiText = """
 				(\\,-------------------------'()'--o
-				(_    _Hard work pays off_    /~" 
+				(_    _Hard work pays off %s_    /~" 
 				(_)_)                   (_)_)
-				""";
+				""".formatted("Emad");
 		System.out.println(asciiText);
 	}
 }
